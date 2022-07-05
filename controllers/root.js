a6f10a895bedecf6041144106c27ef4d4f3a7964
vajpayeemrudul <mrudulrcoem@gmail.com>
2022-07-05 13:19:17 +0530

    test

--- controllers/root.js
+++ controllers/root.js
@@ -1,35 +1,26 @@
 import Admin from "../models/admin.js";
 import ServiceProvider from "../models/serviceProvider.js";
 import Customer from "../models/customer.js";
-import ServiceProvider from "../models/serviceProvider.js";
 
 export const authenticateUser = async (req, res) => {
   let credentials = {
     username: req.body.username,
     password: req.body.password
   }
-<<<<<<< HEAD
    console.log(credentials);
-=======
-  console.log(credentials);
-
->>>>>>> parent of d78f526... Authenticate User
   let customerData = await Customer.find({credential: credentials});
-
   if(customerData.length!=0) {
-    let serviceProviderData = await ServiceProvider.find({customerId: customerData[0].id.valueOf()});
     res.status(200).json({
       message: 'Ok',
-      position: (serviceProviderData.length ? "ServiceProvider": "Customer"),
-      customerData: customerData[0],
-      serviceProviderData: serviceProviderData[0] || ""
+      id: customerData[0].id,
+      position: 'Customer'
     })
+    return;
   }
-  else {
-    res.status(200).json({
-      message: 'Incorrect UserId or Password'
-    })
-  }
+
+  res.status(200).json({
+    message: 'Incorrect UserId or Password'
+  })
 }
 
 // export const createAccount = async (req, res) => {
